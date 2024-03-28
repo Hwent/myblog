a888d258b09d6bbd4092bf28e6adce1927205b73
Wentao <wentaowilliam@outlook.com>
2024-03-27 23:10:03 -0400

    Add login

--- src/components/layout.js
+++ src/components/layout.js
@@ -39,6 +39,11 @@ const Layout = ({ pageTitle, children }) => {
               Blog
             </Link>
           </li>
+          <li className={navLinkItem}>
+            <Link to="/login" className={navLinkText}>
+              Login
+            </Link>
+          </li>
         </ul>
       </nav>
       <main>
