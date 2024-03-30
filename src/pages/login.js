9ef93c2bb3b10306a21c7f5ea22e1421af029841
Wentao <wentaowilliam@outlook.com>
2024-03-29 23:30:04 -0400

    update log in page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,2 +1,16 @@
 import * as React from "react";
 import { Link } from "gatsby";
+
+import Layout from "../components/layout";
+import Seo from "../components/seo";
+
+const LoginPage = () => (
+  <Layout>
+    <Seo title="Login" />
+    <h1>Login</h1>
+    <p>Welcome back! Please login to your account.</p>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+);
+
+export default LoginPage;
