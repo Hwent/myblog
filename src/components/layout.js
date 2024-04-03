b7f232ac83016f6c1b8d7f6ccb1eb2ae82c86248
Wentao <wentaowilliam@outlook.com>
2024-04-02 22:07:46 -0400

    finish login page

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,6 +7,7 @@ import {
   navLinkItem,
   navLinkText,
   siteTitle,
+  error,
 } from "./layout.module.css";
 
 const Layout = ({ pageTitle, children }) => {
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,16 +1,71 @@
 import * as React from "react";
-import { Link } from "gatsby";
+import { Link, navigate } from "gatsby";
 
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 
-const LoginPage = () => (
-  <Layout>
-    <Seo title="Login" />
-    <h1>Login</h1>
-    <p>Welcome back! Please login to your account.</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-);
+const LoginPage = () => {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [errorMessage, setErrorMessage] = React.useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch("http://localhost:3000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      // Handle successful login here
+      console.log("User logged in", data);
+      navigate("/blog");
+    } else {
+      // Handle login failure here
+      console.error("Login failed", data.message);
+      setErrorMessage(data.message);
+    }
+  };
+
+  return (
+    <Layout>
+      <Seo title="Login" />
+      <h1>Login</h1>
+      <p>Welcome back! Please login to your account.</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label>
+          Password
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+      {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+      <p>
+        Don't have an account? <Link to="/signup">Sign up</Link>
+      </p>
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  );
+};
 
 export default LoginPage;
